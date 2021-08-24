c2672a84a397c7f8f0c5107e099a83af85aa7c17
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-08-24 18:10:24 +0700

    xu ly chay banner

--- app/src/main/java/com/example/countdowntimer20072021/MainActivity.java
+++ app/src/main/java/com/example/countdowntimer20072021/MainActivity.java
@@ -5,30 +5,60 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mBtnStartBanner;
+    ImageView mImg;
+    int[] mArrImages = {
+            R.drawable.banner1,
+            R.drawable.banner2,
+            R.drawable.banner3,
+            R.drawable.banner4,
+            R.drawable.banner5
+    };
+    int mIndex = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//
-//
-//        CountDownTimer countDownTimer = new CountDownTimer(5000,1000) {
-//            @Override
-//            public void onTick(long l) {
-//                // TH1 : Nếu l bé hơn < 1000 : không nhảy vào (dưới api 27 )
-//                // Th2 : Nếu l bé hơn < 1000 : vẫn nhảy vào (trên api 27 )
-//                if (l >= 1000){
-//                    Log.d("BBB",l + "");
-//                }
-//            }
-//
-//            @Override
-//            public void onFinish() {
-//
-//            }
-//        };
-//        countDownTimer.start();
+
+        mBtnStartBanner = findViewById(R.id.buttonStartBanner);
+        mImg = findViewById(R.id.imageViewBanner);
+
+        // Task : chạy các hình banner từ tầm đầu tới tấm cuối
+        //      + Nếu tấm cuối thì sẽ quay tấm đầu và chạy tiếp
+
+
+        mBtnStartBanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CountDownTimer countDownTimer = new CountDownTimer(1200 , 1000) {
+                    @Override
+                    public void onTick(long l) {
+                        if (l >= 1000){
+                            if (mIndex >= mArrImages.length){
+                                mIndex = 0;
+                            }
+                            mImg.setImageResource(mArrImages[mIndex]);
+                            mIndex++;
+                        }
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        this.start();
+                    }
+                };
+                countDownTimer.start();
+            }
+        });
+
+
+
+
     }
 }
